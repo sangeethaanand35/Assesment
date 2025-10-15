1fe6c726dd4ea595569ce9425fcf273c44ddc497
sangeethaanand35 <sangeethaanand3597@gmail.com>
2025-10-15 20:09:56 +0530

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        public static string IsPalindrome(string input)
+        {
+            string cleaned = new string(input
+                .Where(char.IsLetterOrDigit)
+                .Select(char.ToLower)
+                .ToArray());
+
+            int left = 0, right = cleaned.Length - 1;
+            while (left < right)
+            {
+                if (cleaned[left] != cleaned[right])
+                    return "No, it's not a palindrome";
+                left++;
+                right--;
+            }
+
+            return "Yes, it's a palindrome";
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the string to check palindrome");
+            string str1 = Console.ReadLine();
+            string result = IsPalindrome(str1);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
---
+++ StringRotation2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRotation2
+{
+    internal class Program
+    {
+      public static string   IsstringRotated(string str1,string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return "two strings are different in length";
+            }
+         
+            char[] rotatedstring= str2.ToArray();
+            Array.Reverse(rotatedstring);
+            string rotatedstring1 = new string(rotatedstring);
+            if (rotatedstring1 == str1)
+            {
+                return "string 1 is rotated version of string 2";
+            }
+            else
+                return "No..two strings are different";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string");
+           string s1= Console.ReadLine();
+            Console.WriteLine("Enter second string");
+            string s2 = Console.ReadLine();
+            string result = IsstringRotated(s1, s2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
